0107cd7a4178bbb29488dfd656705eae42971fb6
Frazer McLean <frazer@frazermclean.co.uk>
2014-12-07 06:25:04 +0100

    Add tests

--- orbital/tests/test_orbital.py
+++ orbital/tests/test_orbital.py
@@ -1,43 +1,23 @@
-from orbital.utilities import mod
-from orbital.elements import KeplerianElements
-from scipy.constants import pi
-import unittest
-import random
-import orbital.bodies
 import math
+import unittest
+
+from numpy import radians
+from orbital.bodies import earth, venus
+from orbital.elements import KeplerianElements
+from orbital.utilities import mod
+from scipy.constants import kilo
 
 
 class TestOrbitalElements(unittest.TestCase):
 
-    def test_mod(self):
-        # These test cases are taken and modified from Python's fmod test
-        # cases. orbital.utilities.mod returns
-        nan = float('NaN')
-        inf = float('Inf')
-        ninf = float('-Inf')
-        self.assertRaises(TypeError, mod)
-        self.assertTrue(math.isnan(mod(nan, 1.)))
-        self.assertTrue(math.isnan(mod(1., nan)))
-        self.assertTrue(math.isnan(mod(nan, nan)))
-        self.assertEqual(mod(1., 0.), 1.)
-        self.assertRaises(ValueError, mod, inf, 1.)
-        self.assertRaises(ValueError, mod, ninf, 1.)
-        self.assertRaises(ValueError, mod, inf, 0.)
-        self.assertEqual(mod(3.0, inf), 3.0)
-        self.assertEqual(mod(-3.0, inf), -3.0)
-        self.assertEqual(mod(3.0, ninf), 3.0)
-        self.assertEqual(mod(-3.0, ninf), -3.0)
-        self.assertEqual(mod(0.0, 3.0), 0.0)
-        self.assertEqual(mod(0.0, ninf), 0.0)
-
     def test_anomaly_at_time(self):
-        orbit = KeplerianElements.orbit_with_period(90 * 60, e=0, body=orbital.bodies.earth)
+        orbit = KeplerianElements.orbit_with_period(90 * 60, e=0, body=earth)
         self.assertAlmostEqual(orbit.M, 0)
         orbit.t += orbit.T
         self.assertAlmostEqual(orbit.M, 0)
 
     def test_apsides(self):
-        orbit = KeplerianElements.orbit_with_period(90 * 60, body=orbital.bodies.earth)
+        orbit = KeplerianElements.orbit_with_period(90 * 60, body=earth)
 
         # Test that only general and specific apside attributes work.
         self.assertRaises(AttributeError, lambda: orbit.apohelion_radius)
@@ -46,13 +26,47 @@ class TestOrbitalElements(unittest.TestCase):
         self.assertEqual(orbit.pericenter_radius, orbit.perigee_radius)
 
         # Ensure earth apsides haven't been added to class definition dynamically.
-        venusian_orbit = KeplerianElements.orbit_with_period(90 * 60, body=orbital.bodies.venus)
+        venusian_orbit = KeplerianElements.orbit_with_period(90 * 60, body=venus)
         self.assertRaises(AttributeError, lambda: venusian_orbit.apogee_radius)
         self.assertRaises(AttributeError, lambda: venusian_orbit.perigee_radius)
 
         # Test multiple apsis names
         self.assertEqual(venusian_orbit.apocenter_radius, venusian_orbit.apocytherion_radius)
         self.assertEqual(venusian_orbit.pericenter_radius, venusian_orbit.perikrition_radius)
+
+    def test_mean_motion(self):
+        """Test setting the mean motion, and its associated effects on a and M0."""
+        orbit = KeplerianElements.orbit_with_altitude(500 * kilo, M0=radians(35), body=earth)
+
+        # Propagate to set t nonzero to test M0 fix when a is set.
+        orbit.propagate_anomaly_by(M=radians(10))
+
+        new_n = 0.5 * orbit.n
+        orbit.n = new_n
+
+        # While we're here, test n set correctly.
+        self.assertAlmostEqual(orbit.n, new_n)
+
+        # Test that fixed M0 allows correct propagation.
+        orbit.propagate_anomaly_to(M=radians(40))
+        self.assertAlmostEqual(orbit.M, radians(40))
+
+    def test_period(self):
+        orbit = KeplerianElements.orbit_with_period(2 * 60 * 60, M0=radians(35), body=earth)
+        self.assertAlmostEqual(orbit.T, 2 * 60 * 60)
+
+        # Propagate to set t nonzero to test M0 fix when a is set.
+        orbit.propagate_anomaly_by(M=radians(10))
+
+        new_T = 3 * 60 * 60
+        orbit.T = new_T
+
+        # While we're here, test n set correctly.
+        self.assertAlmostEqual(orbit.T, new_T)
+
+        # Test that fixed M0 allows correct propagation.
+        orbit.propagate_anomaly_to(M=radians(40))
+        self.assertAlmostEqual(orbit.M, radians(40))
 
 if __name__ == '__main__':
     unittest.main()
