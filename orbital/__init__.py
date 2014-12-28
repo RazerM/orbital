4ac183a006955099ccb51de3340b301219b56118
Frazer McLean <frazer@frazermclean.co.uk>
2014-12-28 06:00:39 +0000

    Bumped version number to 0.5.0.dev0

--- orbital/__init__.py
+++ orbital/__init__.py
@@ -1,6 +1,6 @@
 from orbital.elements import KeplerianElements
 
 __author__ = 'Frazer McLean <frazer@frazermclean.co.uk>'
-__version__ = '0.4.0'
+__version__ = '0.5.0.dev0'
 __license__ = 'MIT'
 __description__ = 'High level orbital mechanics package.'
