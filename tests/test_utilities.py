52f7cab09d0f4ba463b9b1778940b89afdeae070
Matt Giuca <matt.giuca@gmail.com>
2025-08-12 15:50:00 +1000

    Added a comprehensive test suite for KeplerianElements.
    Adds at least one test case for all methods and property getters and setters.
    Tests specifically address edge cases and cases that are handled differently by
    the underlying code (e.g. inclined vs non-inclined, circular vs non-circular
    orbits).
    
    Also introduces a new low-level case in test_utilities for the complicated
    function eccentric_anomaly_from_mean.
    
    Many of these cases are either expecting failure or commented out due to known
    bugs, which have been filed on GitHub and linked from the appropriate tests. The
    commented-out assertions demonstrate the correct expected behaviour once those
    bugs are fixed.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,6 +1,8 @@
 import math
 import unittest
+from math import fmod, radians, tau
 
+from orbital import utilities
 from orbital.utilities import mod
 
 
@@ -25,3 +27,28 @@ class TestUtilities(unittest.TestCase):
         self.assertEqual(mod(-3.0, ninf), -3.0)
         self.assertEqual(mod(0.0, 3.0), 0.0)
         self.assertEqual(mod(0.0, ninf), 0.0)
+
+    def test_eccentric_anomaly_from_mean(self):
+        # Pairs of (e, E) to test round-trips.
+        CASES = [
+            (0.0, 0.0),
+            (0.0, radians(90)),
+            (0.0, radians(270)),
+            (0.75, 0.0),
+            (0.75, radians(90)),
+            (0.75, radians(270)),
+            (0.75, radians(450)),
+            (0.75, radians(-90)),
+            (0.75, 0.000001),
+            (1.0, radians(90)),
+            (1.0, radians(270)),
+        ]
+        # These functions should round-trip, no matter what value is given (as
+        # long as e <= 1.0).
+        for i, (e, E) in enumerate(CASES):
+            M = utilities.mean_anomaly_from_eccentric(e, E)
+            self.assertAlmostEqual(
+                utilities.eccentric_anomaly_from_mean(e, M),
+                fmod(E, tau),
+                msg=f"Case #{i:d}: ({e:f}, {E:f})",
+            )
